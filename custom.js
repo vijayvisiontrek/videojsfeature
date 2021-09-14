767c87c601ba784ab06ed99df00b2a103156af3a
vijay <vijay.kumar@visiontrek.com>
2021-09-14 17:15:46 +0530

    add plug in

--- custom.js
+++ custom.js
@@ -1,20 +1,32 @@
 
      let count=0;
-         document.querySelector('#prev').addEventListener('dblclick', ()=>{ backward()});
+     let prev=   document.querySelector('#prev');
+     let next=   document.querySelector('#next');
+         document.querySelector('#prev').addEventListener('dblclick', ()=>{
+             
+             prev.classList.add('show-seek');
+             backward()
+             setTimeout(() => {
+                 prev.classList.remove('show-seek');
+             }, 2000);
+        });
  
          document.querySelector('#next').addEventListener('dblclick',()=>{
              
-            forward()}
-            
-            );
+            next.classList.add('show-seek');
+            forward()
+                 
+            setTimeout(() => {
+                next.classList.remove('show-seek');
+            }, 2000);   
+        });
  
          let videoplayer=videojs('my-video',{
              controls:true,
-              loop:true,
-             playbackRates:[0.5,1,1.5,2,2.5,3,3.5]
+              loop:false,
+             playbackRates:[0.5,1,1.5,2,2.5,3,3.5],
+             
              });
-           
- 
              videoplayer.seekButtons({
                  forward: 30,
                  back: 10
@@ -23,9 +35,7 @@
  
              const  skip=(time)=>
              {
- 
                  videoplayer.currentTime(videoplayer.currentTime()+time);
- 
              }
  
              const forward=()=>{
@@ -35,3 +45,28 @@
              const backward=()=>{
                  skip(-10)
              }
+             const nextplay=()=>
+             {
+
+                videoplayer.src({
+                    src: "https://content.madfunny.co.za/videos/big_buck_bunny_720p_1mb.mp4",
+                    type:'video/mp4'
+
+                })
+
+                videoplayer.play();
+
+             }
+               const getTitle=()=>
+               {
+                   return "NExt Play ! ";
+               } 
+                const nextt=()=>{
+                    alert("END");
+                }
+
+             videoplayer.upnext({
+                timeout : 5000,
+                getTitle : getTitle,
+                next : nextt
+              });
